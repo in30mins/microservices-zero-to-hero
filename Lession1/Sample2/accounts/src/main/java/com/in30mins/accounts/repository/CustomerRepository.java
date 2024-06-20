7ce7569e822b439b956f275543da03e4707422ae
Raj <raj.jobinfo97@gmail.com>
2024-06-20 14:23:23 +0530

    add basic projects

---
+++ Lession1/Sample2/accounts/src/main/java/com/in30mins/accounts/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.in30mins.accounts.repository;
+
+import com.in30mins.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+}
